2e3e681280360eb7bfcd6dbcadf95360e0f66532
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:32:38 +0200

    rebuild of workshop code

---
+++ system/core/workshop/class.js
@@ -0,0 +1,25 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+
+
+module.exports=function(BOS){
+  return class BlacksmithWorkshop extends BOS{
+    //static counter=0;
+    initializationOfObject(initOpts){
+      //this.archive=//new BOS.Archive(`${this.name}-archive`);
+      //this.forge=//new BOS.Forge(`${this.name}-forge`);
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+
+  };
+
+
+}
