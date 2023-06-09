bcf82a02c876c52478156d49662b5c39f49e818b
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:29:06 +0200

    rebuild of project code

---
+++ system/core/project/class.js
@@ -0,0 +1,18 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+module.exports=function(BOS){
+  return class BlacksmithProject extends BOS.Subject{
+    initializationOfObject(){
+
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+  };
+}
