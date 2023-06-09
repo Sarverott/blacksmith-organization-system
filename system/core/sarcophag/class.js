491098e5cdc3474d8e1a97edc44998575f74ed44
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:29:54 +0200

    rebuild of sarcophag code

---
+++ system/core/sarcophag/class.js
@@ -0,0 +1,18 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+module.exports=function(BOS){
+  return class BlacksmithSarcophag extends BOS{
+    initializationOfObject(){
+
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+  };
+}
