b0c0eb286b65eaeb1d8b7499b2ac4a83f25ed9ef
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:31:21 +0200

    rebuild of superproject code

---
+++ system/core/superproject/class.js
@@ -0,0 +1,25 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+//const {BOS}=require("./../main.js");
+module.exports=function(BOS){
+  return class BlacksmithSuperproject extends BOS.Subject{
+    initializationOfObject(){
+      this.children=[];
+      this.description={};
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+  }
+}
+//BOS.SET_RAPORT(
+//  BlacksmithSuperproject,
+//  require("./raport-superproject.js")
+//);
+//module.exports=BlacksmithSuperproject;
