20f6a8024e07ec5320e63286b95f347fbd34fe9c
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:32:06 +0200

    rebuild of throwbox code

---
+++ system/core/throwbox/class.js
@@ -0,0 +1,27 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+module.exports=function(BOS){
+  return class BlacksmithThrowbox extends BOS.Subject{
+    initializationOfObject(){
+
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+  }
+}
+/*
+const {BOS}=require("./../main.js");
+
+BOS.SET_RAPORT(
+  BlacksmithThrowbox,
+  require("./raport-throwbox.js")
+);
+module.exports=BlacksmithThrowbox;
+*/
