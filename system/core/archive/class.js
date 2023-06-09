f23e8ad55f310708150260bb553e5dce9c783a6a
Sett Sarverott <sarverott@gmail.com>
2023-06-09 09:27:18 +0200

    rebuild of BOS archive code

---
+++ system/core/archive/class.js
@@ -0,0 +1,25 @@
+/*
+  blacksmith-organization-system
+  part of Anubis System
+  Sett Sarverott 2019
+*/
+module.exports=function(BOS){
+  return class BlacksmithArchive extends BOS{
+    initializationOfObject(){
+
+    }
+    loadConfiguration(){
+
+    }
+    eventListenersSetup(){
+
+    }
+  }
+}
+/*
+BOS.SET_RAPORT(
+  BlacksmithArchive,
+  require("./raport-archive.js")
+);
+module.exports=BlacksmithArchive;
+*/
