0d42cfd7802c8bfb2eb6491a19f3760a35089108
Sett Sarverott <sarverott@gmail.com>
2024-01-12 22:58:58 +0100

    keeping notes from previous versions

--- main.js
+++ main.js
@@ -31,3 +31,75 @@ if(require.main===module){
 }else{
   module.exports=require("./system/core/main.js").BOS.INITIALIZE;
 }
+/*
+<<<<<<< HEAD
+const {BOS}=require("./system/core/main.js");
+
+if(require.main===module){
+  const os=require("os");
+
+  var factory=new BOS.Workshop("workshop-name");
+
+  //factory.loadConfiguration("./tmp-enviroment/config");
+  //factory.archivesSetup(...factory.configuration.main.archives);
+  //factory.forgesSetup(...factory.configuration.main.forges);
+
+  //factory.archivesSetup({
+  //  name:"main-archive",
+  //  path:`${os.homedir()}/tmp-enviroment/workspace/arch`
+  //});
+
+  //factory.forgesSetup({
+  //  name:"main-forge",
+  //  path:`${os.homedir()}/tmp-enviroment/workspace/forg`
+  //});
+
+  factory.forgesSetup(
+    {
+      name:"small-A1-forge",
+      path:`/path/to/forge`
+    },
+    {
+      name:"fresh-A9-forge",
+      path:`/path/to/forge`
+    },
+    {
+      name:"old-A7-parted-forge",
+      path:`/path/to/forge`
+    }
+=======
+if(require.main===module){
+  const path = require('path');
+  const fs = require('fs');
+
+  const invokingChain=process.argv.slice(1).filter(
+    (str)=>str!=__filename
+>>>>>>> e9cef81889f757436e4ec20c1c5da7eec61a57a1
+  );
+  //console.log(process.argv);
+
+  if(
+    fs.existsSync(
+      path.join(
+        __dirname,
+        `invoke-${invokingChain[0]}.js`
+      )
+    )
+  ){
+    require(
+      path.join(
+        __dirname,
+        `invoke-${invokingChain[0]}.js`
+      )
+    )(invokingChain.slice(1));
+  }else{
+    console.error("UNKNOWN METHOD: ", invokingChain[0]);
+    console.log("full command argumenting chain: ", invokingChain);
+    console.log("full command: ", process.argv);
+  }
+  //console.log(invokingChain);
+  //
+}else{
+  module.exports=require("./system/core/main.js").BOS.INITIALIZE;
+}
+*/
