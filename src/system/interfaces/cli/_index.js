fb7d451c993498d19c2158283f9b188091422224
Sett Sarverott <sarverott@gmail.com>
2024-10-18 10:18:40 +0200

    making some changes in interfaces

--- src/system/interfaces/cli/_index.js
+++ src/system/interfaces/cli/_index.js
@@ -1,4 +1,34 @@
-const readline = require('readline');
-const repl = require('repl');
-const fs = require('fs');
-const path = require('path');
+const readline = require("readline");
+const repl = require("repl");
+const fs = require("fs");
+const path = require("path");
+
+const BOS = require("../../core/main.js");
+
+function INIT() {
+  this.config = Object.assign(
+    { input: process.stdin, output: process.stdout },
+    this.context.CONFIG.main.interfaces.cli
+  );
+  this.hook = repl.start(this.config);
+  this.hook.setupHistory(this.config.historyPath, HISTORY);
+  for (var command in this.context.COMMANDS) {
+    addCommand(this.hook, command, this.context.COMMANDS[command], this.context);
+  }
+  return cliHook;
+}
+function HISTORY() {}
+
+function addCommand(cliHook, commandname, commandItem) {
+  cliHook.defineCommand(commandname, {
+    help: commandItem.indexData.help,
+    action(...args) {
+      commandItem.CALL(args, this, cliHook);
+      //this.clearBufferedCommand();
+      //console.log(`Hello, ${name}!`);
+      //this.displayPrompt();
+    },
+  });
+}
+
+module.exports=INIT;
