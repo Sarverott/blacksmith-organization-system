ceaaeebc5c78e3417f55e7bdeacbdb17928c9eaf
Sett Sarverott <sarverott@gmail.com>
2024-10-18 13:26:56 +0200

    deployer and scope controll appears

--- src/system/controllers/ScopeControll.js
+++ src/system/controllers/ScopeControll.js
@@ -1,4 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
 const Controller = require("../core/controller.js");
+const BOS =  require("../core/main.js");
+
+/*
+Workshop
+_Forge
+__Superproject
+___Project
+___Sheme
+___Throwbox
+_Archive
+__Sarcophag
+__Exhibit
+_Scrapbook
+__Chaptergroup
+__Mixnotes
+_Setup
+
+
+*/
+
+function SAFE_CREATE_DIR(dirpath){
+  fs.mkdirSync(path.normalize(dirpath),{recursive:true})
+}
 
 class ScopeControll extends Controller {
   LOAD() {}
